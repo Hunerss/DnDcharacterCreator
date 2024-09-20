940c640046fe6bf82221f81e95426edc76ff2a52
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-20 23:16:42 +0200

    Adds working BackgroudnCreator

--- UserControls/BackgroundCreator.xaml.cs
+++ UserControls/BackgroundCreator.xaml.cs
@@ -23,11 +23,34 @@ namespace DnDcharacterCreator.UserControls
     {
         MainWindow window;
         Character character;
+        Random rnd = new();
         public BackgroundCreator(MainWindow win, Character character)
         {
             InitializeComponent();
             window = win;
             this.character = character;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            AlignmentComboBox.SelectedIndex = rnd.Next(0, 9);
+            BackgroundComboBox.SelectedIndex = rnd.Next(0, 12);
+        }
+
+        private void go_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem cbi = AlignmentComboBox.SelectedItem as ComboBoxItem;
+
+            character.Alignment = cbi.Content.ToString();
+            cbi = BackgroundComboBox.SelectedItem as ComboBoxItem;
+            character.Background = cbi.Content.ToString();
+            character.Description = description.Text;
+            character.PersonalityTraits = personality.Text;
+            character.Ideals = ideals.Text;
+            character.Bonds = bonds.Text;
+            character.Flaws = flaws.Text;
+            character.About = about.Text;
+            window.frame.NavigationService.Navigate(new SkillsCreator(window, character));
         }
     }
 }
