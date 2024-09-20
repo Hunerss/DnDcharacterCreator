9235405bf33faa3d84ae92a26c8526538dbad4ce
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-20 23:16:21 +0200

    Adds new fields to Character class

--- Classes/Character.cs
+++ Classes/Character.cs
@@ -9,11 +9,19 @@ namespace DnDcharacterCreator.Classes
     public class Character
     {
         public string Name { get; set; }
-        public string Description { get; set; }
         public string Race { get; set; }
         public string Class { get; set; }
         public string SubClass { get; set; }
         public string Spellcasting { get; set; }
         public Stats Stats { get; set; }
+        public string Alignment { get; set; }
+        public string Background { get; set; }
+        public string Description { get; set; }
+        public string PersonalityTraits { get; set; }
+        public string Ideals { get; set; }
+        public string Bonds { get; set; }
+        public string Flaws { get; set; }
+        public string About { get; set; }
+
     }
 }
