4f83ccd3d46c3aaea6682f39d4d72667f8b1c6ed
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-20 23:55:45 +0200

    Expands skills class

--- Classes/Skills.cs
+++ Classes/Skills.cs
@@ -8,5 +8,23 @@ namespace DnDcharacterCreator.Classes
 {
     class Skills
     {
+        public bool Acrobatics { get; set; }
+        public bool Animal_Handling { get; set; }
+        public bool Arcana { get; set; }
+        public bool Athletics { get; set; }
+        public bool Deception { get; set; }
+        public bool History { get; set; }
+        public bool Insight { get; set; }
+        public bool Intimidation { get; set; }
+        public bool Investigation { get; set; }
+        public bool Medicine { get; set; }
+        public bool Nature { get; set; }
+        public bool Perception { get; set; }
+        public bool Performance { get; set; }
+        public bool Persuasion { get; set; }
+        public bool Religion { get; set; }
+        public bool Sleight_of_Hand { get; set; }
+        public bool Stealth { get; set; }
+        public bool Survival { get; set; }
     }
 }
