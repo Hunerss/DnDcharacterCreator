a5d099921c82aac21e1348abcec9bc63962b31b7
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-22 17:15:07 +0200

    Adds invetory class

---
+++ Classes/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDcharacterCreator.Classes
+{
+    class Inventory
+    {
+        public int Gold {  get; set; }
+        public List<string> Items { get; set; }
+    }
+}
