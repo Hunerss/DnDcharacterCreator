a2a7b3b476e94399f316985414bd1d16e50469a5
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-22 17:15:40 +0200

    Adds frame for loading up character

--- UserControls/MainMenu.xaml.cs
+++ UserControls/MainMenu.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace DnDcharacterCreator.UserControls
 {
@@ -27,14 +28,31 @@ namespace DnDcharacterCreator.UserControls
             InitializeComponent();
         }
 
-        private void new_character_Click(object sender, RoutedEventArgs e)
+        private void New_character_Click(object sender, RoutedEventArgs e)
         {
             window.frame.NavigationService.Navigate(new CharacterCreator(window));
         }
 
-        private void load_character_Click(object sender, RoutedEventArgs e)
+        private void Load_character_Click(object sender, RoutedEventArgs e)
         {
-            //window.frame.NavigationService.Navigate(new MainMenu(this));
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DnDCharacterCreator");
+
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "Character",
+                DefaultExt = ".xml",
+                Filter = "XML File (.xml)|*.xml",
+                InitialDirectory = folderPath
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true) 
+            {
+                string fileName = dialog.FileName;
+
+                //window.frame.NavigationService.Navigate(new MainMenu(this));                
+            }
         }
     }
 }
