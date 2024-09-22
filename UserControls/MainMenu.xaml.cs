b844a7a15693f5507badeb2100775830a21dd070
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-22 18:21:45 +0200

    Adds full navigation

--- UserControls/MainMenu.xaml.cs
+++ UserControls/MainMenu.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DnDcharacterCreator.Classes;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 using Path = System.IO.Path;
 
 namespace DnDcharacterCreator.UserControls
@@ -50,8 +53,17 @@ namespace DnDcharacterCreator.UserControls
             if (result == true) 
             {
                 string fileName = dialog.FileName;
-
-                //window.frame.NavigationService.Navigate(new MainMenu(this));                
+                try
+                {
+                    XmlSerializer serializer = new(typeof(Character));
+                    using StreamReader reader = new(fileName);
+                    Character character = (Character)serializer.Deserialize(reader);
+                    window.frame.NavigationService.Navigate(new Summary(window, character));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error loading character: {ex.Message}");
+                }
             }
         }
     }
